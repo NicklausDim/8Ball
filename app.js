881ca98f4bab7cbe713fbfd79bb043073277e1ac
Nicklaus Dimitriadis <68229696+NicklausDim@users.noreply.github.com>
2021-02-04 14:07:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,39 +1,24 @@
-function dataReceiver() {
-    document.querySelector('#search-results').innerHTML = '';
-    async function dataHandler(data) {
-        let result = await data.json();
-        console.log(result);
-        for (let index = 0; index < result.d.length; index++) {
-            const movie = result.d[index];
-            let card = `
-            <div class="card">
-                <img src = "${movie.i.imageUrl}"
-            </div>
-            `
-            document.querySelector('#search-results').insertAdjacentHTML('beforebegin',card);
-        }
+import dataReceiver from "./Components/dataReceiver.js";
+
+import topMoviesReceiver from "./Components/topMovies.js"
+topMoviesReceiver();
+
+import resetButton from "./Components/resetButton.js"
+
+let urlSearch = 'https://imdb8.p.rapidapi.com/title/auto-complete?q='+document.getElementById('search').value;
+
+let urlTopMovies = 'https://imdb8.p.rapidapi.com/title/get-most-popular-movies?homeCountry=US&purchaseCountry=US&currentCountry=US'
+let options = {
+    'method':'GET',
+    'headers': {
+        'x-rapidapi-key': "ad560d2758mshea3a392b9b06bb4p1dd47fjsnfd9c3537b79f",
+        'x-rapidapi-host': "imdb8.p.rapidapi.com"
     }
+};
+//dataReceiver(urlTopMovies,options);
+document.getElementById("centerDiv").addEventListener("click", function(){dataReceiver(urlSearch,options)});
 
-    function errorHandler(err) {
-        console.log(err);
-    }
+document.getElementById("reset").addEventListener("click", function(){resetButton()});
 
-    let url = 'https://imdb8.p.rapidapi.com/title/auto-complete?q='+document.getElementById('search').value;
-    let options = {
-        'method':'GET',
-        'headers': {
-            'x-rapidapi-key': "",
-            'x-rapidapi-host': "imdb8.p.rapidapi.com"
-        }
-    };
 
-    fetch(url, options).then(dataHandler).catch(errorHandler);
-}
 
-function resetButton() {
-    document.getElementById("search").value="";
-    document.getElementById("grid").innerHTML='<div id="centerDiv" class="card" onclick="dataReceiver()"><img class = "shake" src="./ball.png"></div><div id="search-results"></div>';
-
-    
-    
-}
